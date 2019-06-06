c493efdcdbf098ae4e3b0ce2bab440b7bd3c7e4e
github username <github email address>
2019-06-06 15:01:45 -0400

    Ali fixed the pictures

--- src/components/HogsContainer.js
+++ src/components/HogsContainer.js
@@ -4,9 +4,13 @@ import React from 'react';
 class HogsContainer extends React.Component{
 
   createHogLis = () => {
-    let img = hog.name.split(" ").map(x => x.toLowerCase()).join("_")
-    return this.props.hogs.map(hog => <li>{hog.name} <img src={require(`./${img}.jpg`)}/></li>)
+    // let img = hog.name.split(" ").map(x => x.toLowerCase()).join("_")
+    return this.props.hogs.map(hog => {
+      let img = hog.name.split(" ").map(x => x.toLowerCase()).join("_")
+      return <li>{hog.name} <img src={require(`../hog-imgs/${img}.jpg`)}/></li>
+    })
   }
+
   render(){
     console.log(this.props.hogs)
     return(
