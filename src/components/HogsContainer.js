96788110fc889ce438d2c781e06748c525325ac6
Robert O'Gara <rgo594@gmail.com>
2019-06-06 16:00:35 -0400

    kev is coo

--- src/components/HogsContainer.js
+++ src/components/HogsContainer.js
@@ -2,12 +2,25 @@ import React from 'react';
 
 
 class HogsContainer extends React.Component{
+  state = {
+    hogDesc:
+  }
+
+
+
+  clickHandler = (e) => {
+    this.props.hogs.map(hog => {
+       if (hog.name === e.target.id){
+         this.state.hogDesc.push()
+       }
+    })
+  }
 
   createHogLis = () => {
     // let img = hog.name.split(" ").map(x => x.toLowerCase()).join("_")
     return this.props.hogs.map(hog => {
       let img = hog.name.split(" ").map(x => x.toLowerCase()).join("_")
-      return <li>{hog.name} <img src={require(`../hog-imgs/${img}.jpg`)}/></li>
+      return <li id={hog.name} onClick={this.clickHandler} className="pigTile">{hog.name} <img src={require(`../hog-imgs/${img}.jpg`)}/></li>
     })
   }
 
